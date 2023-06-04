533b5216359508b162e6b769d70e5da46c1dbfb7
joel-lbth <110038598+joel-lbth@users.noreply.github.com>
2023-06-04 21:15:05 +0100

    Create rss_to_sqlite.py

---
+++ rss_to_sqlite.py
@@ -0,0 +1,31 @@
+import feedparser
+import sqlite_utils
+
+# RSS feed URL
+FEED_URL = "https://nitter.net/LutfurRahmanTH/rss"
+
+# SQLite database file
+DATABASE_FILE = "data.db"
+
+# Table name for storing RSS feed items
+TABLE_NAME = "rss_items"
+
+# Connect to the SQLite database
+db = sqlite_utils.Database(DATABASE_FILE)
+
+# Create the table if it doesn't exist
+db[table_name].create({"id": int, "title": str, "link": str}, pk="id", foreign_keys=[])
+
+# Fetch the RSS feed
+feed = feedparser.parse(FEED_URL)
+
+# Upsert the latest records
+for entry in feed.entries:
+    record = {
+        "id": int(entry.id),
+        "title": entry.title,
+        "link": entry.link
+    }
+    db[table_name].upsert(record, pk="id")
+
+print("RSS feed upserted to SQLite successfully.")
