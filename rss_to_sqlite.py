07a916e2f5cc9f94cdf1349ec482a69b5de9cb37
joel-lbth <110038598+joel-lbth@users.noreply.github.com>
2023-06-05 23:26:46 +0100

    Update rss_to_sqlite.py

--- rss_to_sqlite.py
+++ rss_to_sqlite.py
@@ -1,4 +1,7 @@
 from feed_to_sqlite import ingest_feed
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
 
 ingest_feed(db="data.db", url="https://nitter.net/lutfurrahmanth/rss", table_name="lutfur_tweets")
 
